6b99734b3c942838117366f43cd689133773813b
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-06-29 23:33:07 -0700

    commit tree 1217197cee018307df89c5005080580549bdd9f2

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
 class Person
   # your code here
+  def initialize(attributes=nil)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+
+# #with a longer list of attributes
+# will assign an arbitrary number of properties on initialization
+# with a different, shorter list of attributes
+#   will also assign an arbitrary number of properties on initialization
